a425b20eb3f177d0c2b383c66fe4398115787c50
SafalSilwal <safalsilwal1998@gmail.com>
2024-08-19 20:44:56 +0545

    mongodb setup with laravel

--- resources/views/Posts/index.blade.php
+++ resources/views/Posts/index.blade.php
@@ -1,13 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-   <h1>Blog Posts</h1>
-   <a href= "{{ route('posts.create')}}" class= "btn btn-primary">Create Post</a>
-   <ul>
-      @foreach ($posts as $post )
-      <li>
-         <a href="{{ route('posts.show', $post->id) }}">{{ $post->title}}
-      </li>
-         
-      @endforeach   </ul>  
+<div class="container">
+    <h1>Blog Posts</h1>
+    <a href="{{ route('posts.create') }}" class="btn btn-primary mb-3">Create Post</a>
+    
+    @if($posts->isEmpty())
+        <p>No posts available.</p>
+    @else
+        <ul class="list-group">
+            @foreach ($posts as $post)
+                <li class="list-group-item">
+                    <a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a>
+                </li>
+            @endforeach
+        </ul>
+    @endif
+</div>
 @endsection
